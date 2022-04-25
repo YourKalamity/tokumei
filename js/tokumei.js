ac5d29600401866e52e848ea2ffda756c3da8c85
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2022-04-25 22:06:23 +0100

    Fix character limit issues

--- js/tokumei.js
+++ js/tokumei.js
@@ -12,23 +12,17 @@ function makeid(length) {
 }
 
 function peer_open_handler(id){
-    console.log('My peer ID is: ' + id);
     main_menu_modal.show();
     let peer_id_input = document.getElementById("PeerID_inputbox");
-    peer_id_input.focus();
-    peer_id_input.select();
     connect_mode = function() {
         main_menu_modal.hide()
         connection_id = peer_id_input.value
         mode = "TOKUMEI-" + connection_id;
-        console.log("Connecting to " + connection_id + "...");
         var conn = peer.connect(mode);
         conn.on('open', function(){message_handler(conn)});
         return false;
     } 
     await_mode = function() {
-        console.log("Hosting mode...");
-        console.log("Awaiting connection....");
         status_object.innerHTML = "Awaiting connection...";
         main_menu_modal.hide()
         await_mode_modal.show()
@@ -46,6 +40,8 @@ function peer_open_handler(id){
     }}
 
 function message_handler (conn) {
+    let message_fieldset = document.getElementById("message_fieldset");
+    message_fieldset.removeAttribute("disabled");
     status_object.innerHTML = "Connected!";
     console.log("Connection opened");
     conn.on('data', function(data){
@@ -53,6 +49,7 @@ function message_handler (conn) {
         messagebox.innerHTML += "<br>PEER: " + data;
         messagebox.scrollTop = messagebox.scrollHeight;
     })
+    conn.on('close', function(){message_closer()});
     send_message = function() {
         message = document.getElementById("user_message").value
         messagebox = document.getElementById("chatbox");
@@ -64,9 +61,14 @@ function message_handler (conn) {
     }
 };
 
+function message_closer() {
+    let message_fieldset = document.getElementById("message_fieldset");
+    message_fieldset.setAttribute("disabled", "disabled");
+    status_object.innerHTML = "Disconnected!";
+}
+
 var id = makeid(5)
 var peer = new Peer(id);
-
 var send_message;
 let status_object = document.getElementById("status");
 var main_menu_modal = new bootstrap.Modal(document.getElementById('main_menu_modal'));
@@ -77,3 +79,23 @@ var await_mode;
 peer.on('open' , function(id) {
     peer_open_handler(id);
 });
+
+peer.on('error', function(err){
+    status_object.innerHTML = "An error has occured";
+});
+
+peer.on('close', function(){
+    status_object.innerHTML = "Connection closed";
+});
+
+peer.on('disconnected', async function(){
+    status_object.innerHTML = "Broker disconnected";
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    peer.reconnect();
+});
+
+window.addEventListener("beforeunload", function(e){
+    peer.destroy();
+});
+
+    
